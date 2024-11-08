4cc32e6edc26ab1fbeb802ec57e8de8cd546215b
Aiden Lapp <lappaiden@gmail.com>
2024-11-07 21:13:01 -0500

    Merged two working files to combine the adafruit feed messenging system with the arduino code to move the stepper

--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -1,10 +1,12 @@
 #include "credentials.h"
-
 #include <ESP8266WiFi.h>
 #include <AdafruitIO_WiFi.h>
 #include <Servo.h>
 #include <Wire.h>
 #include <RTClib.h>
+#include <Stepper.h>
+
+#define STEPS_PER_REVOLUTION 32
 
 /** 
  * Adafruit IO for handling requests away from the network 
@@ -14,23 +16,28 @@
 AdafruitIO_WiFi io(AIO_USERNAME, AIO_KEY, WIFI_SSID, WIFI_PASS);
 AdafruitIO_Feed *thermostat_feed = io.feed("thermostat");
 
-/** Servo object used to control the switch */
-Servo servo;
 
 /** Real time clock object for scheduling */
 RTC_DS3231 rtc;
 
-/** Instantiate cool and off positions as defined in thermostat.h */
-const int cool_position = 90;
-const int off_position = 0;
+Stepper stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
+
+const int stepsToCool = 1000;
+const int stepsToHeat = -1000;
+
+enum Mode { OFF, COOL, HEAT };
+Mode currentMode = OFF;
+
+int currentPosition = 0;
+
 
 
 void setup() {
-  /** Begin serial communication */
-  Serial.begin(115200);
-
-  /** Initialize servo */
-  servo.attach(9);
+  Serial.println(" ");
+  stepper.setSpeed(400);
+  currentMode = OFF;
+  currentPosition = 0;
+  Serial.begin(9600); 
 
   /** Connect to cloud service  */
   io.connect();
@@ -61,15 +68,68 @@ void handler(AdafruitIO_Data *data) {
   last_command = command;
 
   if (command == "cool") {
-    servo.write(cool_position);
+    changeModeToCool();
     Serial.println("Thermostat set to cool.");
   }
   else if (command == "off") {
-    servo.write(off_position);
+    changeModeToOff();
     Serial.println("Thermostat set to off.");
   }
   else {
     Serial.println("Command not found");
   }
+  
 }
 
+
+
+void changeModeToCool() {
+  if (currentMode != COOL) {
+    stepper.step(stepsToCool);
+    currentPosition += stepsToCool;
+  } else if (currentMode == COOL) {
+    Serial.println("Already in Cool mode.");
+    return;
+  }
+  currentMode = COOL;
+  delay(1000);
+  returnToNeutral();
+  Serial.println("Mode changed to Cool and returned to neutral.");
+}
+
+void changeModeToHeat() {
+  if (currentMode != HEAT) {
+    stepper.step(stepsToHeat);
+    currentPosition += stepsToHeat;
+  } else if (currentMode == HEAT) {
+    Serial.println("Already in Heat mode.");
+    return;
+  }
+  currentMode = HEAT;
+  delay(1000);
+  returnToNeutral();
+  Serial.println("Mode changed to Heat and returned to neutral.");
+}
+
+void changeModeToOff() {
+  if (currentMode == COOL) {
+    stepper.step(-stepsToCool);
+    currentPosition += -stepsToCool;
+  } else if (currentMode == HEAT) {
+    stepper.step(-stepsToHeat);
+    currentPosition += -stepsToHeat;
+  } else if (currentMode == OFF) {
+    Serial.println("Already in Off mode");
+    return;
+  }
+  currentMode = OFF;
+  delay(1000);
+  returnToNeutral();
+  Serial.println("Mode changed to Off and returned to neutral.");
+}
+
+void returnToNeutral() {
+  int stepsToNeutral = -currentPosition;
+  stepper.step(stepsToNeutral);
+  currentPosition = 0;
+}
