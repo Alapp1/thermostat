897447de331f7980ba2fbb47a91ab10b7008a140
Aiden Lapp <lappaiden@gmail.com>
2024-12-23 02:14:22 -0500

    fixed 404 error for static files

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
