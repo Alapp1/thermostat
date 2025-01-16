66c67868f9101f819fa70bfaafe32a2c0c39c110
Aiden Lapp <lappaiden@gmail.com>
2025-01-16 01:03:27 -0500

    capitalization is important

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
