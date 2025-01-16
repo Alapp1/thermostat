702e79348b2750049e75450dfa8755cebd2d3085
mattmello939 <mattmello939@gmail.com>
2025-01-16 18:52:37 -0500

    Refactoring file structure

--- controllers/logoutController.js
+++
@@ -1,6 +0,0 @@
-export const logoutUser = (req, res) => {
-  res.clearCookie('jwt', { httpOnly: true, sameSite: 'strict', secure: process.env.NODE_ENV === 'production' });
-  res.clearCookie('csrfToken', { sameSite: 'strict', secure: process.env.NODE_ENV === 'production' });
-  res.status(200).json({ message: 'Logged out successfully' });
-};
-
